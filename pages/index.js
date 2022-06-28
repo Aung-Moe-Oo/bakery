96de7e0752ec79c6364ea063508546abd8a99b9c
Aung-Moe-Oo <70925607+Aung-Moe-Oo@users.noreply.github.com>
2022-06-28 19:12:37 +0630

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,6 @@ export default function Home({ cakeList }) {
       <Head>
         <title>Aung Cake Shop in Myanmar</title>
         <meta name="description" content="Best Cake Shop" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&family=Quicksand:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
       <Featured />
       <CakeList cakeList={cakeList} />
